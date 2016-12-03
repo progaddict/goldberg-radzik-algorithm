926dfbda0d512ff6c4310d1f89f404cce059e6d2
Alex Ryndin <ryndin.as@gmail.com>
2016-12-03 15:14:26 +0100

    got webpack working

---
+++ webpack.config.babel.js
@@ -0,0 +1,21 @@
+import webpack from 'webpack';
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: './dist'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    }
+}
